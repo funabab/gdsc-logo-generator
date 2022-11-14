73c7ead92b7797d1778fad974ed2191985b81065
Abdulbassit Abdullahi Funsho <cs.kwasu2019@gmail.com>
2022-11-14 22:40:26 +0100

    fix: added patch for esbuild issue with yoga-prebuilt and satori

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // HACK FIX: for yoga-prebuilt issue with satori
-  // https://github.com/vercel/satori/issues/185#issuecomment-1278264083
-  define: {
-    _a: 'undefined',
-  },
 })
